57cd3f1ec2aea2f41104c2d7d4d907376297bac9
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-08-04 12:46:37 +0300

    feat: Update QuickActions component to use dynamic routing for action links

--- src/components/dashboard/home/QuickActions.jsx
+++ src/components/dashboard/home/QuickActions.jsx
@@ -5,21 +5,25 @@ export default function QuickActions() {
     {
       icon: "/icons/customize.svg",
       title: "General Settings",
+      to: "general-settings",
       desc: "Manage your app's terms, policies, and slider content",
     },
     {
       icon: "/icons/user-plus.svg",
       title: "Providers Requests",
+      to: "requests",
       desc: "Review and approve new service provider registrations",
     },
     {
       icon: "/icons/reservations.svg",
       title: "Reservations",
+      to: "bookings",
       desc: "View and manage all user bookings and schedules",
     },
     {
       icon: "/icons/support-messages.svg",
       title: "Support Messages",
+      to: "support",
       desc: "Respond to user inquiries and support requests",
     },
   ];
@@ -32,7 +36,7 @@ export default function QuickActions() {
       <div className="actions_links">
         {actions.map((action, index) => (
           <Link
-            to="/dashboard/general-settings"
+            to={`/dashboard/${action.to}`}
             className="action_link"
             key={index}
           >
